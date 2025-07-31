cd8210295de794e00458a854a8857eeb37de3237
Sebas Cardozo 97 <109832396+codify97@users.noreply.github.com>
2025-07-31 18:14:33 -0300

    Add files via upload

---
+++ JavaProyectoTienda/com/techlab/app/Main.java
@@ -0,0 +1,10 @@
+package com.techlab.app;
+
+import com.techlab.menu.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar(); // Asegura que este método se llame
+    }
+}
